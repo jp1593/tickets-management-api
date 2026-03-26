e4acbcd8d452cfd5e5fd3f66aa6bb55d8c01de2a
jp1593 <juanpablo08082002@hotmail.com>
2026-03-26 10:52:00 -0600

    feat: add cors configuration to backend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRoutes = require('./routes/api'); 
+
+app.use(cors());
 
 app.use(express.json()); // Middleware for Json parse
 
