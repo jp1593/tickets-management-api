f8f730fede1833f9e3eddba85290fdabb39ffb8a
jp1593 <juanpablo08082002@hotmail.com>
2026-03-26 15:39:02 -0600

    feat: add dashboard route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ticketController = require('../controllers/ticket.controller');
-const paymentController = require('../controllers/payment.controller');
+const ticketController = require("../controllers/ticket.controller");
+const paymentController = require("../controllers/payment.controller");
 
 // Tickets
-router.get('/tickets', ticketController.getTickets);
-router.get('/tickets/:id', ticketController.getTicketById);
+router.get("/tickets", ticketController.getTickets);
+router.get("/tickets/:id", ticketController.getTicketById);
 
 // Bonus-Payments
-router.get('/payments/summary', paymentController.getWeeklyPaymentSummary);
+router.get("/payments/summary", paymentController.getWeeklyPaymentSummary);
+
+// Dashboard
+router.get("/payments/dashboard-stats", paymentController.getDashboardStats);
 
 module.exports = router;
