cb6b76507d059c7eb6e1fb65aefa3a040bdcd679
jp1593 <juanpablo08082002@hotmail.com>
2026-03-27 16:21:31 -0600

    feat: add controller for deletion to routes

--- src/routes/tickets.routes.js
+++ src/routes/tickets.routes.js
@@ -5,5 +5,6 @@ const ticketController = require("../controllers/ticket.controller");
 router.get("/", ticketController.getTickets);
 router.get("/:id", ticketController.getTicketById);
 router.post("/", ticketController.createTicket);
+router.delete("/:id", ticketController.deleteTicket);
 
 module.exports = router;
